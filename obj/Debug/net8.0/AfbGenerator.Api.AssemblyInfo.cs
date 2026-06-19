80d77561502388c9240cf04dac66f3c64308d6c0
mariem202 <mariemmhiri82@gmail.com>
2026-06-19 17:25:50 +0100

    CHANGES

--- obj/Debug/net8.0/AfbGenerator.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/AfbGenerator.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AfbGenerator.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5651edbf3f8f57cdb33d4883dbaa45f8e88a5b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a7d37e0c32dd3204937e80f7c1926f067a062fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AfbGenerator.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AfbGenerator.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
